7e600dc4ef8184e273b5a1ae297ff2e912bbaeac
Mizanur Rahman <2018-1-60-249@std.ewubd.edu>
2023-03-29 04:59:09 +0600

    function adder

---
+++ ind.js
@@ -0,0 +1,10 @@
+//find min from a array
+function findMin(arr){
+    var min = arr[0];
+    for(var i = 1; i < arr.length; i++){
+        if(arr[i] < min){
+            min = arr[i];
+        }
+    }
+    return min;
+}
